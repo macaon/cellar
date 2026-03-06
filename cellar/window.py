d2f07d094fbae88d9568a9dffccb5008e0f98ca1
macaon <marcus@skogangen.se>
2026-03-06 21:19:35 +0100

    fix: use io.github.cellar icon in About dialog

--- cellar/window.py
+++ cellar/window.py
@@ -449,8 +449,8 @@ class CellarWindow(Adw.ApplicationWindow):
     def _on_about_activated(self, _action, _param) -> None:
         dialog = Adw.AboutDialog(
             application_name="Cellar",
-            application_icon="application-x-executable",
-            version="0.42.56",
+            application_icon="io.github.cellar",
+            version="0.42.57",
             comments="A GNOME storefront for Windows and Linux apps.",
             license_type=Gtk.License.GPL_3_0,
         )

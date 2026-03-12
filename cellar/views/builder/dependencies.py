a697cf9d61cb40842f0e16c93da9336362856a05
macaon <marcus@skogangen.se>
2026-03-12 23:11:24 +0100

    feat: add Wine Mono and Gecko to dependency picker
    Adds a "Wine Components" category with mono (.NET compatibility) and
    gecko (HTML/IE engine) as installable winetricks verbs. GE-Proton
    bundles both but they aren't installed into prefixes by default.

--- cellar/views/builder/dependencies.py
+++ cellar/views/builder/dependencies.py
@@ -99,6 +99,10 @@ _VERB_CATALOGUE: list[tuple[str, list[tuple[str, str]]]] = [
         ("xna31", "Microsoft XNA Framework 3.1"),
         ("xna40", "Microsoft XNA Framework 4.0"),
     ]),
+    ("Wine Components", [
+        ("mono",  "Wine Mono (.NET compatibility layer)"),
+        ("gecko", "Wine Gecko (HTML/Internet Explorer engine)"),
+    ]),
 ]
 
 
